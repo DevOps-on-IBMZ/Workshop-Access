7e8a8938915e80081a520057997f761f85da874a
kenishia <kenishia@us.ibm.com>
2023-11-06 16:34:06 -0500

    Update wildfire-labs.js
    Updated for new spreadsheet

--- wildfire-labs.js
+++ wildfire-labs.js
@@ -35,17 +35,17 @@ function getLab(email) {
     var table_data = '<div class="alert alert-warning" role="alert">Email not found.</div>'
     if (index != -1) {
         table_data =  '<p>Below is the information you will need to access the exercises:</p>';
-        table_data += '<h4>Main DevOps Lab Exercises: </h4>';
+        table_data += '<h4 style="color:blue;">Main DevOps Lab Exercises: </h4>';
         table_data += '<h5>Open a web browser </h5>';
         table_data += '<p>Enter this URL address in the browser’s URL area: <br><mark>'+lines[index][1]+'</mark></p>';
         table_data += '<p>Authentication with username: <mark>'+lines[index][2]+'</mark></p>';
-        table_data += '<p>Using password: <mark>'+lines[index][5]+'</mark></p>';
+        table_data += '<p>Using password: <mark>'+lines[index][3]+'</mark></p>';
         
         table_data += '<h4>ADDI Lab Exercises: </h4>';
         table_data += '<h5>Open a web browser </h5>';
-        table_data += '<p>Enter this URL address in the browser’s URL area: <br><mark>'+lines[index][6]+'</mark></p>';
-        table_data += '<p>Authentication with username: <mark>'+lines[index][2]+'</mark></p>';
-        table_data += '<p>Using password: <mark>'+lines[index][5]+'</mark></p>';
+        table_data += '<p>Enter this URL address in the browser’s URL area: <br><mark>'+lines[index][4]+'</mark></p>';
+        table_data += '<p>Authentication with username: <mark>'+lines[index][5]+'</mark></p>';
+        table_data += '<p>Using password: <mark>'+lines[index][6]+'</mark></p>';
 
         table_data += '<h5>Exiting the lab</h5>';
         table_data += '<p><strong>IMPORTANT: <br>Please DO NOT shutdown remote Windows system, just close the session. Otherwise you won&apos;t be able to reconnect.</strong></p>';
